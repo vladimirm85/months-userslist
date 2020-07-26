a5a795db1d211fd5e55def9f410251f9c15546a6
vladimirm85 <vladimirm85@gmail.com>
2020-07-26 21:35:51 +0300

    UsersList styling

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import { UserInterface } from '../../App';
+
+const useStyles = makeStyles({
+  table: {
+    width: '800px',
+  },
+  item: {
+    textAlign: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
 
 interface UsersListPropsInterface {
   users: UserInterface[];
@@ -8,14 +28,32 @@ interface UsersListPropsInterface {
 export const UsersList: React.FC<UsersListPropsInterface> = (
   props: UsersListPropsInterface
 ): JSX.Element => {
+  const classes = useStyles();
+
   return (
-    <React.Fragment>
-      {props.users.map((user: UserInterface, index: number) => (
-        <div key={index}>
-          First Name: {user.firstName}, Last Name: {user.lastName}, birthday:{' '}
-          {user.dob.toLocaleDateString()}
-        </div>
-      ))}
-    </React.Fragment>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className={classes.item}>#</TableCell>
+            <TableCell className={classes.text}>First name</TableCell>
+            <TableCell className={classes.text}>Last name</TableCell>
+            <TableCell className={classes.text}>Birthday</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.users.map((user: UserInterface, index: number) => (
+            <TableRow key={user.id}>
+              <TableCell className={classes.item}>{index + 1}</TableCell>
+              <TableCell className={classes.text}>{user.firstName}</TableCell>
+              <TableCell className={classes.text}>{user.lastName}</TableCell>
+              <TableCell className={classes.text}>
+                {user.dob.toLocaleDateString()}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
